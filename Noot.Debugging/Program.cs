f1578576154094cd01baf2aa3e4ff7e5eb8dc231
unknown <rlombard@minerp.com>
2020-07-16 17:39:13 +0200

    init code

---
+++ Noot.Debugging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Noot.Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
