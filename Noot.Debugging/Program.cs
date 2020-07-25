7027336fc35b181e876ca932e0f147547d862141
unknown <rlombard@minerp.com>
2020-07-25 10:42:55 +0200

    Removed unused code

--- Noot.Debugging/Program.cs
+++ Noot.Debugging/Program.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using Noot.EntityFramework;
 using Microsoft.EntityFrameworkCore;
-using Noot.DataAccess.Database;
-using Noot.DataAccess.Models;
 
 namespace Noot.Debugging
 {
     class Program
     {
         static void Main(string[] args)
-        {             
+        {
+            
         }
     }
 }
